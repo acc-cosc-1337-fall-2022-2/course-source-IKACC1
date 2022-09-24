261dbc11659e56664952177b783a0597013882c2
IKACC1 <imrath.keith@g.austincc.edu>
2022-09-24 13:44:01 +0000

    Homework 3-decisions with test cases.

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,46 @@
 //write include statements
+#include "decisions.h"
+#include <string>
+#include <iostream>
+using std::cout;
+using std::cin;
 
-int main() 
+
+int main()
 {
+int grade;
+int menu_option; 
+std::string the_letter_grade;
+std::cout<<"MAIN MENU \n"<<"1-Letter grade using if\n"<<"2-Letter grade using switch\n"<<"3-Exit\n";
+std::cin>>menu_option;
+switch(menu_option)
+{
+case 1:
+	std::cout<<"Please enter the grade:	";
+	std::cin>>grade;
+	if (grade >=00&& grade<=100)
+		{
+		the_letter_grade = get_letter_grade_using_if(grade);
+		std::cout<<"Letter Grade is:	"<<the_letter_grade<<"\n";
+		}
+		else
+		std::cout<<"You entered a number that is out of range!"<<std::endl;
+		
+	break;
+case 2: 
+	std::cout<<"Please enter the grade:	";
+	std::cin>>grade;
+	if (grade >=00&& grade<=100)
+	{
+		the_letter_grade = get_letter_grade_using_switch(grade);
+		std::cout<<"Letter Grade is:	"<<the_letter_grade<<"\n";
+	}
+	else 
+	std::cout<<"You entered a number that is out of range!"<<std::endl;
+	break;
+case 3: 
 	return 0;
+}
+
+return 0;
 }
