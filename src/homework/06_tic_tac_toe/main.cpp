be29d99d0aa034dc243cabb22b26eaab92a5fea6
IKACC1 <imrath.keith@g.austincc.edu>
2022-10-29 23:49:37 -0500

    Homework 6  with test cases

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,32 @@
-
+#include "tic_tac_toe.h"
+#include <iostream>
 int main() 
 {
+	LOOP: string player_choice;
+	std::cout<<"\n First Player! Please Enter (Capital) X or O:\t";//instructions specify capital  X or O not lowercase x or o, so I only allow those.
+	std::cin>>player_choice;
+	TicTacToe game;// instantiate
+	if(player_choice!="X"&&player_choice!="O")//instructions say argument value must be X or O, so I Loop if not X or O.
+		{
+			std::cout<<"Enter (Capital) X or O to continue";
+			goto LOOP;
+		}
+	game.start_game(player_choice);
+	do //inner loop for marking board
+	{	
+		int position;
+		std::cout<<"\n What position do you wish to play? choose 1-9:\t";
+		std::cin>>position;
+		game.mark_board(position);
+		game.display_board();
+
+	}
+	while(game.game_over()==false);
+	string continue_check;
+	std::cout<<"\nDo you want to continue or exit? if you want to continue press y:\t";
+	std::cin>>continue_check;
+	if(continue_check=="y")//if input ==y, control will go to beginning, clear board and begin game again.
+		goto LOOP;
 
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,59 @@
 //cpp
+#include "tic_tac_toe.h"
+#include <iostream>
+#include <iomanip>
+void TicTacToe::start_game(string first_player)
+{
+if (first_player=="X"||first_player=="O")
+   {
+        TicTacToe::player= first_player;
+        TicTacToe::clear_board(); 
+   } 
+}
+
+void TicTacToe::mark_board(int position)
+{
+    pegs[position-1]=player;
+    TicTacToe::set_next_player();
+}
+string TicTacToe::get_player()const
+{
+    return TicTacToe::player;
+}
+
+void TicTacToe::display_board()const
+  {
+    for(int i=0; i<9; i++)
+    {   if (i==3||i==6)
+           {std::cout<<"\n";}
+        std::cout<<pegs[i];
+        if(i!=2&&i!=5&&i!=8)
+            std::cout<<"|";
+    }
+    }
+  void TicTacToe::set_next_player()
+  {
+    if(TicTacToe::player=="X")
+        TicTacToe::player="O";
+    else
+    player="X";
+  }
+
+  bool TicTacToe::check_board_full()
+  {
+    bool space_checker=true;
+    for(int i=0; i<9;i++)
+            if(TicTacToe::pegs[i]==" ")
+                space_checker=false;
+    return space_checker;
+  }
+   void TicTacToe::clear_board()
+   {
+        for(int i=0; i<9;i++)
+                (TicTacToe::pegs[i])=" ";
+   }
+
+  bool TicTacToe::game_over()
+  {
+      return check_board_full();
+  }
